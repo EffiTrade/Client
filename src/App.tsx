18aba4e6cf8cd03b158f6cb8a2d39356f72b94fd
Kacper Mazur <kacper5103@gmail.com>
2023-10-14 00:36:04 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
 
 function App() {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:4000');
+    setSocket(newSocket);
+
+    newSocket.on('orderResult', (data) => {
+        console.log('Order Result:', data);
+    });
+
+    newSocket.on('orderError', (error) => {
+        console.error('Order Error:', error);
+    });
+
+    return () => {
+        newSocket.close(); // Disconnect socket when component unmounts
+    };
+  }, []);
+
+  const handleOrder = () => {
+    if (socket) {
+      // Emitting order request to the backend
+      socket.emit('placeOrder', {
+        symbol: 'BTCUSDT',
+        quantity: 0.001,
+        price: 50
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleOrder}>Place Order</button>
     </div>
   );
 }
